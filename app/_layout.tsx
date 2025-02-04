9b6b89e77c9091cb40013a8425c6a63dfabd8b01
ShihiroMori <131643237+ShihiroMori@users.noreply.github.com>
2025-02-04 08:00:18 +0000

    v0.1

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,16 +3,21 @@ import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { SafeAreaView, SafeAreaViewBase, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { ThemedText } from '@/components/ThemedText';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+
+  const [counter, setCounter] = useState(0)
+
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
@@ -27,13 +32,37 @@ export default function RootLayout() {
     return null;
   }
 
+  const handlePress = () => {
+    setCounter((val) => val += 1)
+  }
+
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
+          <View style={styles.container}>
+            <TouchableOpacity onPress={handlePress}>
+              <Text style={styles.btn}>dddddddddd</Text>
+            </TouchableOpacity>
+            <Text style={styles.text}>
+              {counter}
+            </Text>
+          </View>
     </ThemeProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    marginTop: 100,
+    flexDirection: "row"
+  },
+  btn:{
+    color: "#000000",
+    backgroundColor: "#00ff00",
+    // fontSize: 400,
+    padding: 50.
+  },
+  text:{
+    color: "darkblue",
+    fontSize: 64
+  }
+});
