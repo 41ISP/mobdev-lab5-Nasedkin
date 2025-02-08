1aaf44147c500f33373fc42c5fa1f071d14cb155
ShihiroMori <131643237+ShihiroMori@users.noreply.github.com>
2025-02-08 04:11:17 +0000

    v.0825

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -25,14 +25,24 @@ export default function RootLayout() {
       state: false
     }
   ]
+  const initialFilter = [
+    {
+      id: nanoid(),
+      state: false
+    }
+  ]
   const [tasks, setTasks] = useState([...initialTasks])
   const colorScheme = useColorScheme();
 
   const [task, setTask] = useState('')
   const toggleSwitch = (id: string) => {
-    setTasks((old) => old.map((tsk) => tsk.id === id? {...tsk, state: !tsk.state}: tsk))
+    setTasks((old) => old.map((tsk) => tsk.id === id ? { ...tsk, state: !tsk.state } : tsk))
   }
-
+  const [activeFilter, setActiveFilter] = useState([...initialFilter])
+  const handleFilterActive = (id: string) => {
+    setActiveFilter((old) => old.map((filt) => filt.id === id ? {...filt, state: !filt.state} : filt))
+  }
+  
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
@@ -49,77 +59,67 @@ export default function RootLayout() {
 
 
   const handleSubmit = () => {
-    if(task.trim().length > 0 && task.trim().length < 20)
-    {
+    if (task.trim().length > 0 && task.trim().length < 20) {
       setTasks([
         ...tasks,
-        {id: nanoid(), task: task.trim(), state: false}
+        { id: nanoid(), task: task.trim(), state: false }
       ])
+      setTask('')
     }
     else alert("Пустая строка, или слишком много символов (допустимо не больше 20)")
   }
 
-  const handlePress = () => {
-    if(task.trim().length > 0 && task.trim().length <= 20)
-    {
-      setTasks([
-        ...tasks,
-        {id: nanoid(), task: task.trim(), state: false}
-      ])
-    }
-    else alert("Пустая строка, или слишком много символов (допустимо не больше 20)")
-  }
-  const handleDelete = (id:string) => {
+  const handleDelete = (id: string) => {
     setTasks(tasks.filter(a => a.id !== id))
-  }
-
-  const [filteredTasks, setFilteredTasks] = useState([...tasks])
-
-  const filteredList = () => {
-    const temp = setFilteredTasks(
-      (tasks.filter(task => {return task.state === false})
-    )
   }
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-          <View style={styles.container}>
-            <TextInput placeholder='👉 Попечатай 👈'
-                       value={task} 
-                       onChangeText={setTask}
-                       onSubmitEditing={handleSubmit}
-                       style = {styles.input}
-                       returnKeyType='done'>
-            </TextInput>
-            <TouchableOpacity onPress={handlePress}>
-              <Text style={styles.btn}>
-              👉 Потрогай 👈
+      <View style={styles.container}>
+        <TextInput placeholder='Введите задачу'
+          value={task}
+          onChangeText={setTask}
+          onSubmitEditing={handleSubmit}
+          style={styles.input}
+          returnKeyType='done'>
+        </TextInput>
+        <View style={styles.switch}>
+          <Text style={styles.text}>
+            Фильтр
+          </Text>
+          <Switch value = {activeFilter}
+                  onValueChange={handleFilterActive}
+                  trackColor={{ false: '#767577', true: '#81b0ff' }}/>
+        </View>
+        <TouchableOpacity onPress={handleSubmit}>
+          <Text style={styles.btn}>
+              Добавить
+          </Text>
+        </TouchableOpacity>
+        <FlatList data={tasks} keyExtractor={(item) => item.id} renderItem={(
+          { item },
+        ) => (
+          <View style={styles.cntr}>
+            <TouchableOpacity onPress={() => handleDelete(item.id)}>
+              <Text style={styles.list}>
+                Х
               </Text>
-            </TouchableOpacity>
-            <FlatList  data={tasks} keyExtractor={(item) => item.id} renderItem={(
-              {item},
-            ) => (
-              <View style={styles.cntr}> 
-                <TouchableOpacity onPress={()=>handleDelete(item.id)}>
-                  <Text style={styles.list}>
-                  👉Х👈
-                  </Text>
-                </TouchableOpacity >
-                <Text style={styles.list}>{item.task}</Text>
-                <Switch value={item.state}
-                        onValueChange={()=>toggleSwitch(item.id)}
-                        thumbColor={item.state ? "yellow" : "red"} 
-                        trackColor={{ false: '#767577', true: '#81b0ff' }}/>
-              </View>
-            )}/>
+            </TouchableOpacity >
+            <Text style={styles.list}>{item.task}</Text>
+            <Switch value={item.state}
+              onValueChange={() => toggleSwitch(item.id)}
+              thumbColor={item.state ? "yellow" : "red"}
+              trackColor={{ false: '#767577', true: '#81b0ff' }} />
           </View>
+        )}/>
+      </View>
     </ThemeProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    marginTop: 200,
+  container: {
+    marginTop: 100,
     flexDirection: "column",
     alignItems: "center",
     alignContent: "center",
@@ -127,34 +127,48 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: "goldenrod",
     backgroundColor: "darkblue",
-    borderRadius: 50,
+    borderRadius: 10,
     minHeight: 300,
     maxWidth: 350,
     minWidth: 350
   },
-  btn:{
+  text:{
+    color: 'goldenrod',
+    fontSize: 14,
+  },
+  switch:{
+    flexDirection: 'row',
+    color: "goldenrod",
+    backgroundColor: "darkred",
+    fontSize: 20,
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: "goldenrod",
+    padding: 10.
+  },
+  btn: {
     color: "goldenrod",
     backgroundColor: "darkred",
     fontSize: 30,
     margin: 20,
-    borderRadius: 50,
+    borderRadius: 10,
     borderWidth: 2,
     borderColor: "goldenrod",
     padding: 20.
   },
-  input:{
+  input: {
     color: "goldenrod",
     fontSize: 32,
     padding: 10,
     textAlign: 'center',
     margin: 20,
     maxWidth: 300,
-    borderRadius: 50,
+    borderRadius: 10,
     borderWidth: 2,
     borderColor: "goldenrod",
     backgroundColor: "darkred"
   },
-  list:{
+  list: {
     color: "goldenrod",
     backgroundColor: "darkred",
     fontSize: 20,
@@ -167,7 +181,7 @@ const styles = StyleSheet.create({
     maxWidth: 150,
     width: "100%"
   },
-  cntr:{
+  cntr: {
     flexDirection: "row",
     flexGrow: 1,
     width: "100%",
@@ -176,3 +190,5 @@ const styles = StyleSheet.create({
     alignSelf: "center",
   }
 });
+
+//👉 Потрогай 👈
